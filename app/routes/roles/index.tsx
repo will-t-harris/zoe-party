f1899fa9cdcf83f8d0bd5d64b4b6a6fa9ed0475b
Will Harris <will.t.harris@protonmail.com>
2023-01-10 21:36:25 -0800

    update roles index page with copy, updated styles, and flickering text animation

--- app/routes/roles/index.tsx
+++ app/routes/roles/index.tsx
@@ -1,10 +1,10 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, Link, Outlet, useLoaderData } from "@remix-run/react";
+import { partition } from "lodash";
 
 import { useUser } from "~/utils";
 import { getAllRoles } from "~/models/role.server";
-import { partition } from "lodash";
 
 export async function loader({ request: _request }: LoaderArgs) {
   const roles = await getAllRoles();
@@ -22,7 +22,7 @@ export default function RolesPage() {
 
   return (
     <div className="flex h-full min-h-screen flex-col">
-      <header className="flex items-center justify-between bg-yellow-100 p-4 text-black">
+      <header className="header-background flex items-center justify-between p-4 text-black">
         <p>{user.email}</p>
         <Form action="/logout" method="post">
           <button
@@ -34,9 +34,53 @@ export default function RolesPage() {
         </Form>
       </header>
 
-      <main className="pb-14">
+      <main className="pb-14 text-white">
+        <h1 className="flickering-text mb-4 mt-20 text-center text-xl">
+          A party to die for
+        </h1>
+        <div className="mx-auto mt-20 max-w-2xl">
+          <p className="mb-2">
+            You've been invited to a murder mystery party set in a 1920's
+            speakeasy. Guests are on a mission to uncover clues, discover
+            secrets, and find the murderer... hopefully before becoming their
+            next victim.
+          </p>
+          <p className="mb-2">
+            <i> Bum bum buuuum!</i>
+          </p>
+          <p className="mb-2">
+            To RSVP: claim a role, don your fanciest 20's attire, and prepare
+            yourself for a night of jazz, cocktails/mocktails, and (scandalous!)
+            rouged knees.
+          </p>
+          <p className="mb-2">
+            Because everyone has secrets to hide, you won't see your full
+            (unredacted) character card until you claim a role.
+          </p>
+          <p className="mb-2">
+            You'll notice that roles are grouped into two categories, primary
+            and secondary.{" "}
+            <strong>
+              If possible, please select from the primary roles until they are
+              all claimed.
+            </strong>{" "}
+          </p>
+          <p className="mb-2">            
+            If there are two people mentioned on your invitation, please log in
+            and choose roles separately, <strong>with separate email addresses.</strong>
+          </p>
+          <p className="mb-2">
+            Your secret-filled role card will be sent to the email address you
+            provide as soon as your role is claimed.
+          </p>
+          <p className="mb-2">
+            The guest who figures out the killer and their motive will win a
+            prize, but of course the true reward is the thrill of the chase.{" "}
+          </p>
+          <p className="mb-2">Can't wait to see you there!</p>
+        </div>
         <div className="flex-flex-col mt-14 mb-8 justify-center">
-          <h2 className="mb-4 text-center text-xl font-extrabold tracking-tight text-white sm:text-4xl lg:text-6xl">
+          <h2 className="mb-4 text-center text-xl font-extrabold tracking-tight sm:text-4xl lg:text-6xl">
             Primary Roles
           </h2>
 
@@ -46,7 +90,7 @@ export default function RolesPage() {
                 <li key={role.id} className="flex content-center">
                   <Link
                     to={`/roles/${decodeURIComponent(role.name)}`}
-                    className="text-white underline visited:text-purple-400"
+                    className="underline visited:text-purple-400"
                   >
                     {role.name}
                   </Link>
@@ -65,7 +109,7 @@ export default function RolesPage() {
           </ul>
         </div>
 
-        <h2 className="text-center text-xl font-extrabold tracking-tight text-white sm:text-4xl lg:text-6xl">
+        <h2 className="mb-4 text-center text-xl font-extrabold tracking-tight sm:text-4xl lg:text-6xl">
           Secondary Roles
         </h2>
 
@@ -75,7 +119,7 @@ export default function RolesPage() {
               <li key={role.id} className="flex content-center">
                 <Link
                   to={`/roles/${decodeURIComponent(role.name)}`}
-                  className="text-white underline visited:text-purple-400"
+                  className="underline visited:text-purple-400"
                 >
                   {role.name}
                 </Link>
